b8548cbb499459a6349f507d37b4c1ef7d80bef8
Cis112233 <christopherpeart@hotmail.com>
2017-01-30 19:05:59 -0500

    First version

---
+++ MemeLang.py
@@ -0,0 +1,43 @@
+
+"""
+« is beginning of string
+» ends string
+
+
+
+
+
+"""
+memeLang(input)
+{
+
+    #Globals    
+    printStr = ""
+    inPrint = false
+    
+
+    #Checking for chars
+    for char in input:
+	if char = "«"
+	    inPrint = false
+        if char = "»"
+	    inPrint = true
+
+
+
+
+	#Handling chars
+	if inPrint && char != "»"
+	    printStr+=char
+        
+
+
+
+
+	#And print output
+        if printStr.length > 0
+	    print printStr
+
+
+
+}
